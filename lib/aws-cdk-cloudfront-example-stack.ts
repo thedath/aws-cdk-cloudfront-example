5c3a598bea292b8b0e6252b6836e45d9cdfe91ad
Thedath Oudarya <thedath.oudarya@gmail.com>
2022-11-19 18:27:32 +0530

    Init code setup
    Signed-off-by: Thedath Oudarya <thedath.oudarya@gmail.com>

--- lib/aws-cdk-cloudfront-example-stack.ts
+++ lib/aws-cdk-cloudfront-example-stack.ts
@@ -1,16 +1,54 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as cloudfront from "aws-cdk-lib/aws-cloudfront";
+import * as origins from "aws-cdk-lib/aws-cloudfront-origins";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as apigateway from "aws-cdk-lib/aws-apigateway";
+import { Cors } from "aws-cdk-lib/aws-apigateway";
 
 export class AwsCdkCloudfrontExampleStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const apiGateway = new apigateway.RestApi(this, "ApiWithWAFEnabled", {
+      restApiName: "ApiWithWAFEnabled",
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsCdkCloudfrontExampleQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const rootLambda = new lambda.Function(this, "ApiGETLambda", {
+      functionName: "ApiGETLambda",
+      runtime: lambda.Runtime.NODEJS_16_X,
+      handler: "root.handler",
+      code: lambda.Code.fromAsset(`lib/lambda`),
+    });
+
+    const testLambda = new lambda.Function(this, "ApiGETLambda", {
+      functionName: "ApiGETLambda",
+      runtime: lambda.Runtime.NODEJS_16_X,
+      handler: "test.handler",
+      code: lambda.Code.fromAsset(`lib/lambda`),
+    });
+
+    apiGateway.root.addCorsPreflight({
+      allowOrigins: Cors.ALL_ORIGINS,
+      allowMethods: ["GET"],
+    });
+    apiGateway.root.addMethod(
+      "GET",
+      new apigateway.LambdaIntegration(rootLambda)
+    );
+
+    apiGateway.root
+      .addResource("test", {
+        defaultCorsPreflightOptions: {
+          allowOrigins: Cors.ALL_ORIGINS,
+          allowMethods: ["GET"],
+        },
+      })
+      .addMethod("POST", new apigateway.LambdaIntegration(testLambda));
+
+    const apiOrigin = new origins.HttpOrigin(apiGateway.url);
+    const cfDistribution = new cloudfront.Distribution(this, "CfDistribution", {
+      defaultBehavior: { origin: apiOrigin },
+    });
   }
 }
