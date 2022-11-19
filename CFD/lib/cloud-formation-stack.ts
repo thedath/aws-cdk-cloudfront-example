174756a4f24f7b94f42402d28d6e1fa34a68c51f
Thedath Oudarya <thedath.oudarya@gmail.com>
2022-11-19 23:39:43 +0530

    Complete CFD configuration
    Signed-off-by: Thedath Oudarya <thedath.oudarya@gmail.com>

--- CFD/lib/cloud-formation-stack.ts
+++ CFD/lib/cloud-formation-stack.ts
@@ -11,9 +11,75 @@ export default class CloudFormationStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props: CloudFormationStackProps) {
     super(scope, id, props);
 
+    // const originGroups = new origins.OriginGroup({});
+
     const apiOrigin = new origins.HttpOrigin(props.originHTTPUrl);
+
+    const cachePolicy = new cloudfront.CachePolicy(this, "CachePolicy", {
+      cachePolicyName: "ExampleCachePolicy",
+      comment: "Cache policy for testing purposes",
+      minTtl: cdk.Duration.seconds(1),
+      maxTtl: cdk.Duration.seconds(1 * 60 * 60 * 24 * 365),
+      defaultTtl: cdk.Duration.seconds(10),
+      enableAcceptEncodingBrotli: true,
+      enableAcceptEncodingGzip: true,
+      headerBehavior: cloudfront.CacheHeaderBehavior.allowList(
+        "x-cache-test1",
+        "x-cache-test2"
+      ),
+      queryStringBehavior: cloudfront.CacheQueryStringBehavior.allowList(
+        "param1",
+        "param2"
+      ),
+      cookieBehavior: cloudfront.CacheCookieBehavior.none(),
+    });
+
+    const originRequestPolicy = new cloudfront.OriginRequestPolicy(
+      this,
+      "OriginRequestPolicy",
+      {
+        originRequestPolicyName: "ExampleOriginRequestPolicy",
+        comment: "Origin request policy for testing purposes",
+        headerBehavior: cloudfront.OriginRequestHeaderBehavior.all(),
+        queryStringBehavior: cloudfront.OriginRequestQueryStringBehavior.all(),
+        cookieBehavior: cloudfront.OriginRequestQueryStringBehavior.none(),
+      }
+    );
+
+    const responseHeadersPolicy = new cloudfront.ResponseHeadersPolicy(
+      this,
+      "ResponseHeadersPolicy",
+      {
+        responseHeadersPolicyName: "",
+        comment: "",
+        corsBehavior: {
+          accessControlAllowCredentials: false,
+          accessControlAllowOrigins: ["*"],
+          accessControlAllowHeaders: ["*"],
+          accessControlAllowMethods: ["*"],
+          accessControlExposeHeaders: [],
+          accessControlMaxAge: cdk.Duration.seconds(600),
+          originOverride: true,
+        },
+        securityHeadersBehavior: {},
+        customHeadersBehavior: {
+          customHeaders: [
+            { header: "Custom-H1", override: true, value: "Custom Value 1" },
+            { header: "Custom-H2", override: true, value: "Custom Value 2" },
+          ],
+        },
+      }
+    );
+
     const cfDistribution = new cloudfront.Distribution(this, "CfDistribution", {
-      defaultBehavior: { origin: apiOrigin },
+      defaultBehavior: {
+        origin: apiOrigin,
+        allowedMethods: cloudfront.AllowedMethods.ALLOW_ALL,
+        cachePolicy,
+        originRequestPolicy,
+        responseHeadersPolicy,
+        cachedMethods: cloudfront.CachedMethods.CACHE_GET_HEAD_OPTIONS,
+      },
     });
   }
 }
