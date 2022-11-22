f7c269cea8fedc8f51fcfe98c48bfd009a72537c
Thedath Oudarya <56229135+thedath@users.noreply.github.com>
2022-11-22 14:17:46 +0530

    Change configs

--- APIG/bin/apig.ts
+++ APIG/bin/apig.ts
@@ -7,10 +7,8 @@ const app = new cdk.App();
 
 new ApiGatewayStack(app, "OriginApiGatewayStack", {
   stackName: "OriginApiGatewayStack",
-  env: { account: "448535425092", region: "us-west-2" },
 });
 
 new ApiGatewayStack(app, "VirginiaApiGatewayStack", {
   stackName: "VirginiaApiGatewayStack",
-  env: { account: "448535425092", region: "us-east-1" },
 });
